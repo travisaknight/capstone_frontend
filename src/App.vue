ab7a7a521462f932ba6a15dc11e05a11edba328a
Travis Knight <travis.a.knight@gmail.com>
2019-08-24 12:42:26 -0500

    Update index and streamlined filesin theme.

--- src/App.vue
+++ src/App.vue
@@ -1,88 +1,136 @@
 <template>
   <div id="app">
-    <header></header>
-    <div id="nav">
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="/">GAINZ</a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNavDropdown"
-          aria-controls="navbarNavDropdown"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <router-link to="/" class="nav-link">Your Workouts</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link to="/exercises" class="nav-link">Exercises</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link to="/signup" class="nav-link">Signup</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link to="/login" class="nav-link">Login</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link to="/logout" class="nav-link">Logout</router-link>
-            </li>
-            <!-- <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdownMenuLink"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                Dropdown link
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <a class="dropdown-item" href="#">Something else here</a>
-              </div>
-            </li> -->
-          </ul>
+    <header class="header header-right">
+      <div class="container-fluid">
+        <!-- Logos-->
+        <div class="inner-header">
+          <a class="inner-brand" href="/">
+            <img class="brand-dark" src="assets/images/logo.png" width="77px" alt="" />
+            <img class="brand-light" src="assets/images/logo-light.png" width="77px" alt="" />
+          </a>
         </div>
-      </nav>
-    </div>
+        <!-- Navigation-->
+        <div class="inner-navigation collapse">
+          <div class="inner-navigation-inline">
+            <div class="inner-nav onepage-nav">
+              <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#portfolio">Portfolio</a></li>
+                <li><a href="#team">Team</a></li>
+                <li><a href="#services">Services</a></li>
+                <li><a href="#blog">Blog</a></li>
+                <li><a class="external" href="start.html">All Demos</a></li>
+                <!-- Button-->
+                <li>
+                  <a class="nav-btn external" href="#">
+                    <div class="btn btn-circle btn-brand">Get started</div>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <!-- Mobile menu-->
+        <div class="nav-toggle">
+          <a href="#" data-toggle="collapse" data-target=".inner-navigation">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </a>
+        </div>
+      </div>
+    </header>
+
     <router-view />
+
+    <footer class="footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 col-lg-3">
+            <!-- Text widget-->
+            <aside class="widget widget_text">
+              <div class="textwidget">
+                <p><img src="assets/images/logo-light.png" width="100" alt="" /></p>
+                <p>
+                  Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut non
+                  enim eleifend felis pretium feugiat.
+                </p>
+                E-mail:
+                <a href="mailto:support@core.com">support@core.com</a>
+                <br />
+                Phone: 8 800 123 4567
+                <br />
+                Fax: 8 800 123 4567
+                <br />
+              </div>
+            </aside>
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <!-- Recent entries widget-->
+            <aside class="widget widget_recent_entries">
+              <div class="widget-title">
+                <h5>Recent Posts</h5>
+              </div>
+              <ul>
+                <li>
+                  <a href="#">Experience the sound of a modern and clean 360° Bluetooth Speaker.</a>
+                  <span class="post-date">May 8, 2016</span>
+                </li>
+                <li>
+                  <a href="#">Experience the sound of a modern and clean 360° Bluetooth Speaker.</a>
+                  <span class="post-date">April 7, 2016</span>
+                </li>
+                <li>
+                  <a href="#">Experience the sound of a modern and clean 360° Bluetooth Speaker.</a>
+                  <span class="post-date">April 7, 2016</span>
+                </li>
+              </ul>
+            </aside>
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <!-- Twitter widget-->
+            <aside class="widget twitter-feed-widget">
+              <div class="widget-title">
+                <h5>Twitter Feed</h5>
+              </div>
+              <div class="twitter-feed" data-twitter="345170787868762112" data-number="1"></div>
+            </aside>
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <!-- Tags widget-->
+            <aside class="widget widget_tag_cloud">
+              <div class="widget-title">
+                <h5>Tags</h5>
+              </div>
+              <div class="tagcloud">
+                <a href="#">Design</a>
+                <a href="#">Travel</a>
+                <a href="#">Startup</a>
+                <a href="#">Music</a>
+                <a href="#">Portfolio</a>
+                <a href="#">Responsive</a>
+              </div>
+            </aside>
+          </div>
+        </div>
+      </div>
+      <div class="footer-copyright">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="text-center">
+                <span class="copyright">
+                  © 2017 Core, All Rights Reserved. Design with love by
+                  <a href="http://2the.me/">2the.me</a>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
-<style>
-#app {
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-
-.navbar-brand {
-  font-family: "Bowlby One SC", cursive;
-  font-size: 50px;
-}
-
-body {
-  font-family: "Arvo", serif;
-  background-image: url(/vintage-concrete.png);
-}
-</style>
+<style></style>
